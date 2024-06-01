7108b2a8cdda108a29ad3b2f6a81238f903c9051
Eliot <eliot.droumaguet28@gmail.com>
2024-06-01 13:30:11 +0200

    TD6 Web : Serveur Web

--- src/App.java
+++ src/App.java
@@ -6,16 +6,13 @@ import java.util.ArrayList;
 import dao.ProductsDAO;
 import database.PolyBayDatabase;
 import models.Product;
+import webserver.WebServer;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            ProductsDAO productsDAO = new ProductsDAO();
-            ArrayList<Product> listOfProducts = productsDAO.findAll();
-
-            for (Product product : listOfProducts) {
-                System.out.println(product);
-            }
+            WebServer webserver = new WebServer();
+            webserver.listen(8080);
         }
         catch (Exception e) {}
     }
